8ec195cdb745df646a0080a618add767561a7baa
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-07-01 11:55:06 +0700

    SET UP EXCEPTION AND CONNECT TO SERVER

---
+++ Phase2TerminalReceiver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Sockets;
+
+class Receiver
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            TcpClient client = new TcpClient("192.168.1.100",5713);
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine("RECEIVER");
+            writer.Flush();
+            stream.Close();
+            writer.Close();
+            client.Close();
+        }catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
