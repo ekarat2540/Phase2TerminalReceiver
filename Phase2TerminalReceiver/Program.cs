5b0aed410a9869f8e8589b92dc24d850c925c0fd
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-07-01 12:03:44 +0700

    GET IP AND PORT FROM SENDER

--- Phase2TerminalReceiver/Program.cs
+++ Phase2TerminalReceiver/Program.cs
@@ -9,11 +9,18 @@ class Receiver
             TcpClient client = new TcpClient("192.168.1.100",5713);
             NetworkStream stream = client.GetStream();
             StreamWriter writer = new StreamWriter(stream);
+            StreamReader streamReader = new StreamReader(stream);
             writer.WriteLine("RECEIVER");
+            string ipAndPort = await streamReader.ReadLineAsync();
+            string split = ipAndPort.Split(":");
+            string ipSender = split[0];
+            int port = split[1];
             writer.Flush();
+            streamReader.Close();
             stream.Close();
             writer.Close();
             client.Close();
+
             Console.WriteLine("TEST");
         }catch (Exception ex)
         {
